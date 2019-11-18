dad568b6b0c2d7f60e055cb7ebc27a2cbfd6a45b
William Lam <teststuffreally@gmail.com>
2019-11-18 20:26:37 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -5,7 +5,7 @@ describe "./variable.rb" do
     greeting = get_variable_from_file('./variable.rb', "greeting")
 
     expect(greeting).to eq("Hello World")
-    Hello World = "Hello World"
+   
   end
 
 end
